b8b2dc8a04330534b0b2d390d3cab926c7426438
Pengfei Liu <261223363@qq.com>
2022-10-20 10:23:08 +0800

    test

---
+++ src/assembly/resources/sbin/compare.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+echo ---------------------
+echo Starting Export
+echo ---------------------
+
+if [ -z "${COMPARE_TOOL_HOME}" ]; then
+  export COMPARE_TOOL_HOME="$(cd "`dirname "$0"`"/..; pwd)"
+fi
+
+if [ -n "$JAVA_HOME" ]; then
+    for java in "$JAVA_HOME"/bin/amd64/java "$JAVA_HOME"/bin/java; do
+        if [ -x "$java" ]; then
+            JAVA="$java"
+            break
+        fi
+    done
+else
+    JAVA=java
+fi
+
+CLASSPATH=""
+for f in ${COMPARE_TOOL_HOME}/lib/*.jar; do
+  CLASSPATH=${CLASSPATH}":"$f
+done
+
+MAIN_CLASS=com.yonyou.iotdb.test.UpgradeTestFrom2To3
+
+"$JAVA" -cp "$CLASSPATH" "$MAIN_CLASS" "$@"
+exit $?
