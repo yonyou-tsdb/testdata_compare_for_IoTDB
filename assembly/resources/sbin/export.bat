e189e151fed879e8baaddb4e4fda897a0df27cc8
Pengfei Liu <261223363@qq.com>
2022-10-20 09:51:10 +0800

    test

---
+++ assembly/resources/sbin/export.bat
@@ -0,0 +1,42 @@
+
+@echo off
+echo ````````````````````````````````````````````````
+echo Starting Export
+echo ````````````````````````````````````````````````
+
+if "%OS%" == "Windows_NT" setlocal
+
+pushd %~dp0..
+if NOT DEFINED COMPARE_TOOL_HOME set COMPARE_TOOL_HOME=%CD%
+popd
+
+if NOT DEFINED MAIN_CLASS set MAIN_CLASS=com.yonyou.iotdb.test.SnapshootIoTDBSummary
+if NOT DEFINED JAVA_HOME goto :err
+
+@REM -----------------------------------------------------------------------------
+@REM JVM Opts we'll use in legacy run or installation
+set JAVA_OPTS=-ea^
+ -DIOTDB_HOME=%COMPARE_TOOL_HOME%
+
+@REM ***** CLASSPATH library setting *****
+set CLASSPATH=%COMPARE_TOOL_HOME%\lib\*
+
+REM -----------------------------------------------------------------------------
+
+"%JAVA_HOME%\bin\java" -DCOMPARE_TOOL_HOME=%COMPARE_TOOL_HOME% %JAVA_OPTS% -cp %CLASSPATH% %MAIN_CLASS% %*
+set ret_code=%ERRORLEVEL%
+goto finally
+
+
+:err
+echo JAVA_HOME environment variable must be set!
+set ret_code=1
+pause
+
+
+@REM -----------------------------------------------------------------------------
+:finally
+
+ENDLOCAL
+
+EXIT /B %ret_code%
